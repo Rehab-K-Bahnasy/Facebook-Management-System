2cb09237e827a3fa6ae9ea984b484eebae69979f
abdllrhmanzedan <abdom.zedan@gmail.com>
2023-12-01 15:57:56 +0200

    Welcome and Login screen added. Bugs: scene not resizable with window

---
+++ src/main/java/welcomeLogin/WelcomeLogin.java
@@ -0,0 +1,27 @@
+package welcomeLogin;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WelcomeLogin extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(WelcomeLogin.class.getResource("WelcomeScene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1440, 1024);
+
+        stage.setTitle("theSocialNetwork");
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
