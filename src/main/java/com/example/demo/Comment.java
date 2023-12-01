a4cae00cf46ea3a9fb117d633d4fe5b9239522e1
MoBakry04 <mowaelbk@gmail.com>
2023-12-01 03:26:12 +0200

    Created The Post Class And Added The Setters And Getters For Both Post And Comment Classes

--- src/main/java/com/example/demo/Comment.java
+++ src/main/java/com/example/demo/Comment.java
@@ -1,4 +1,64 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+
 public class Comment {
+
+    private long comment_ID;
+    private long post_ID;
+    private long user_ID;
+    private String [] comment_content = new String[10000];
+    //private User [] comment_react = new User[10000];
+    private LocalDate comment_created_on;
+    private Comment [] comment_reply = new Comment[10000];
+
+    public void setComment_ID(long comment_ID) {
+        this.comment_ID = comment_ID;
+    }
+
+    public void setComment_content(String[] comment_content) {
+        this.comment_content = comment_content;
+    }
+
+    public void setComment_created_on(LocalDate comment_created_on) {
+        this.comment_created_on = comment_created_on;
+    }
+
+    public void setComment_reply(Comment[] comment_reply) {
+        this.comment_reply = comment_reply;
+    }
+
+    public long getPost_ID() {
+        return post_ID;
+    }
+
+    public Comment[] getComment_reply() {
+        return comment_reply;
+    }
+
+    public LocalDate getComment_created_on() {
+        return comment_created_on;
+    }
+
+    public long getComment_ID() {
+        return comment_ID;
+    }
+
+    public long getUser_ID() {
+        return user_ID;
+    }
+
+    public String[] getComment_content() {
+        return comment_content;
+    }
+
+    void addComment(Comment comment) {
+        this.comment_content=comment.comment_content;
+        this.user_ID=comment.user_ID;
+        this.comment_created_on=comment.comment_created_on;
+        comment_ID++;
+
+    }
+
+
 }
---
+++ src/main/java/com/example/demo/Post.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import java.time.LocalDate;
+public class Post {
+
+    private long post_ID;
+    private LocalDate post_created_on;
+    private String [] post_caption = new String[10000];
+    private int post_reacts;
+    //private User [] users_tagged_in_post = new User[10000];
+    private Comment[] comments_on_post = new Comment[10000];
+    private String post_privacy;
+
+    public void setPost_reacts(int post_reacts) {
+        this.post_reacts = post_reacts;
+    }
+
+    public void setComments_on_post(Comment[] comments_on_post) {
+        this.comments_on_post = comments_on_post;
+    }
+
+    public void setPost_caption(String[] post_caption) {
+        this.post_caption = post_caption;
+    }
+
+    public void setPost_ID(long post_ID) {
+        this.post_ID = post_ID;
+    }
+
+    public void setPost_privacy(String post_privacy) {
+        this.post_privacy = post_privacy;
+    }
+
+    public void setPost_created_on(LocalDate post_created_on) {
+        this.post_created_on = post_created_on;
+    }
+
+    public int getPost_reacts() {
+        return post_reacts;
+    }
+
+    public Comment[] getComments_on_post() {
+        return comments_on_post;
+    }
+
+    public String[] getPost_caption() {
+        return post_caption;
+    }
+
+    public long getPost_ID() {
+        return post_ID;
+    }
+
+    public String getPost_privacy() {
+        return post_privacy;
+    }
+
+    public LocalDate getPost_created_on() {
+        return post_created_on;
+    }
+
+
+}
