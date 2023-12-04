6c3f9273b4e87aea2050b28ede9260c5f4c12e89
abdllrhmanzedan <abdom.zedan@gmail.com>
2023-12-04 14:08:30 +0200

    Finalized welcome/login design Added: login validation, show password box and create account

--- src/main/java/welcomeLogin/WelcomeLoginController.java
+++ src/main/java/welcomeLogin/WelcomeLoginController.java
@@ -1,32 +1,66 @@
 package welcomeLogin;
 
+import dataManager.DataManager;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import user.User;
 
 import java.net.URL;
+import java.nio.channels.AlreadyBoundException;
 import java.util.ResourceBundle;
 
-public class WelcomeLoginController{
+public class WelcomeLoginController {
 
     @FXML
-    TextField usernameField;
+    private TextField emailField;
     @FXML
-    PasswordField passwordField;
-    public void revealPassword() {
-
+    private PasswordField passwordField;
+    @FXML
+    private TextField passwordText;
+    @FXML
+    private CheckBox showPassword;
+    public void passwordVisibility() {
+        if(showPassword.isSelected())
+        {
+            passwordText.setText(passwordField.getText());
+            passwordText.setVisible(true);
+            passwordField.setVisible(false);
+            return;
+        }
+        passwordField.setText(passwordText.getText());
+        passwordField.setVisible(true);
+        passwordText.setVisible(false);
     }
 
     public void login() {
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-
+        String inputEmail = emailField.getText();
+        String inputPassword = passwordField.getText();
+        if(showPassword.isSelected())
+            inputPassword = passwordText.getText();
+        Alert alert;
+        if(!DataManager.isValidUserCredentials(inputEmail,inputPassword))
+        {
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Wrong login credentials");
+            alert.setHeaderText("We couldn't find an account with that username.\n" +
+                    "Please check your credentials and try again.");
+            alert.showAndWait();
+            return;
+        }
+        alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Welcome back ya m3lm");
+        alert.setHeaderText("Welcome back ya m3lm");
+        alert.showAndWait();
+        //else: switch scene to the user home page
     }
 
     public void createNewAccount() {
-        
+
     }
 }
