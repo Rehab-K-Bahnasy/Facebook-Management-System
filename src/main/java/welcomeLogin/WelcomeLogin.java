6cc7e543c3f95c8a499e63633ca7ec08ed92a07d
abdllrhmanzedan <abdom.zedan@gmail.com>
2023-12-04 16:01:37 +0200

    -Added: create account package and scene(not finished)

--- src/main/java/welcomeLogin/WelcomeLogin.java
+++ src/main/java/welcomeLogin/WelcomeLogin.java
@@ -3,6 +3,7 @@ package welcomeLogin;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -13,7 +14,8 @@ public class WelcomeLogin extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(WelcomeLogin.class.getResource("WelcomeScene.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1024, 720);
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 1024, 720);
 
         stage.setTitle("SOCIO");
         stage.setScene(scene);
