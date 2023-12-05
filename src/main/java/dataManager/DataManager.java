c676e2b65e72be191a018a19369ceff25bdfe141
abdllrhmanzedan <abdom.zedan@gmail.com>
2023-12-05 04:48:35 +0200

    updates and fixes to User and Person. added: DataWriter,DataReader and users.json Reading and Writing users data: DONE
    Signed-off-by: abdllrhmanzedan <abdom.zedan@gmail.com>

--- src/main/java/dataManager/DataManager.java
+++ src/main/java/dataManager/DataManager.java
@@ -2,75 +2,17 @@ package dataManager;
 
 import user.User;
 
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
-import java.io.File;
-import java.io.FileWriter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.ArrayList;
 
-public class DataManager
-{
-    //key:username,value:user
-    protected static Map<String,User> users = new HashMap<>();
-    private static final String usersFilePath = "src\\main\\resources\\dataManager\\usersData.txt";
-    public static void readUsers()
-    {
-        try
-        {
-            File file = new File(usersFilePath);
-            Scanner in = new Scanner(file);
+public class DataManager {
+    protected static ArrayList<User> users = new ArrayList<>();
+    protected final static String USERS_FILE_PATH = "src\\main\\resources\\dataManager\\users.json";
 
-            //userData structure: username email password firstName lastName phoneNumber birthDate gender
-            while (in.hasNextLine())
-            {
-                String line = in.nextLine();
-                System.out.println(line);
-                String[] userData = line.split(" ");
-                users.put(userData[0],new User(userData));
-            }
-            in.close();
+    public static User retrieveUser(String identifier) {
+        for (var user : users) {
+            if (user.hasMatchingIdentity(identifier))
+                return user;
         }
-        catch (Exception exp)
-        {
-            System.out.println(exp.getMessage());
-        }
-    }
-    public static void writeUsers()
-    {
-        //first open the file and write nothing to empty it
-        //then write the whole new data
-        try
-        {
-            File file = new File("src\\main\\resources\\dataManager\\usersData.txt");
-            FileWriter out = new FileWriter(file);
-
-            out.close();
-        }
-        catch (Exception exp)
-        {
-            System.out.println(exp.getMessage());
-        }
-    }
-
-    //this should be the validator, and should extend the data manger
-    public static boolean isValidUserCredentials(String inputEmail,String inputPassword)
-    {
-        if(!users.containsKey(inputEmail))
-        {
-            return false;
-        }
-        var retrievedUser = users.get(inputEmail);
-        return retrievedUser.checkPasswordMatch(inputPassword);
-    }
-    public static User retrieveUser(String email)
-    {
-        return users.get(email);
-    }
-
-    public static void main(String[] args)
-    {
-        JSONObject user = new JSONObject();
+        return null;
     }
 }
--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -2,22 +2,21 @@ package user;
 
 import org.json.simple.JSONObject;
 
-import java.util.Arrays;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
-public class User extends Person{
+public class User extends Person {
     private String username;
     private String email;
-    private String username;
     private String password;
-    public User(String[] userData) {
-        //sending userData from 4th (3rd index) element to person constructor
-        //firstName lastName phoneNumber birthDate gender
-        super(Arrays.copyOfRange(userData,3,userData.length));
-        setUsername(userData[0]);
-        setEmail(userData[1]);
-        setPassword(userData[2]);
+
+    public User(Map userData) {
+        super(userData);
+        setUsername((String) userData.get("username"));
+        setEmail((String) userData.get("email"));
+        setPassword((String) userData.get("password"));
     }
 
     public String getUsername() {
@@ -31,27 +30,71 @@ public class User extends Person{
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public boolean checkPasswordMatch(String inputPassword) {
         return password.equals(inputPassword);
     }
-    public JSONObject toJsonObject()
-    {
-        Map<String,String> userMapping = new HashMap<>();
-        userMapping.put("username",username);
-        userMapping.put("email",email);
-        userMapping.put("password",password);
-        userMapping.put("first name",firstName);
-        userMapping.put("last name",lastName);
-        userMapping.put("phone number",phoneNumber);
-        userMapping.put("birth date",birthDate.toString());
-        userMapping.put("gender",gender);
+
+    public boolean checkUsernameMatch(String inputUsername) {
+        return username.equals(inputUsername);
+    }
+
+    public boolean checkEmailMatch(String inputEmail) {
+        return username.equals(inputEmail);
+    }
+
+    public boolean checkPhoneNumberMatch(String inputPhoneNumber) {
+        return username.equals(inputPhoneNumber);
+    }
+
+    public boolean hasMatchingIdentity(String identifier) {
+        return checkUsernameMatch(identifier) ||
+                checkEmailMatch(identifier) ||
+                checkPhoneNumberMatch(identifier);
+    }
+
+    public JSONObject toJsonObject() {
+        Map<String, Object> userMapping = new HashMap<>();
+        userMapping.put("username", username);
+        userMapping.put("email", email);
+        userMapping.put("password", password);
+        userMapping.put("first name", firstName);
+        userMapping.put("last name", lastName);
+        userMapping.put("phone number", phoneNumber);
+        userMapping.put("birthdate", birthDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+        userMapping.put("gender", gender);
 
         return new JSONObject(userMapping);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", birthDate=" + birthDate +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        //checking username is enough, as a username, email and phone number only assigned once
+        return Objects.equals(username, user.username);
     }
 }
