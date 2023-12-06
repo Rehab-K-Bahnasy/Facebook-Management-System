53d6a45496127561edb4c4bdef995ff552622eac
abdllrhmanzedan <abdom.zedan@gmail.com>
2023-12-06 04:42:59 +0200

    Added switching scenes between login and create account
    Signed-off-by: abdllrhmanzedan <abdom.zedan@gmail.com>

--- src/main/java/welcomeLogin/WelcomeLogin.java
+++ src/main/java/welcomeLogin/WelcomeLogin.java
@@ -1,25 +1,27 @@
 package welcomeLogin;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class WelcomeLogin extends Application {
+    public static Stage mainStage;
+    public static Parent root;
+    public static Scene scene;
+    public static FXMLLoader fxmlLoader;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(WelcomeLogin.class.getResource("WelcomeScene.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 1024, 720);
-
+        fxmlLoader = new FXMLLoader(WelcomeLogin.class.getResource("WelcomeScene.fxml"));
+        root = fxmlLoader.load();
+        scene = new Scene(root, 1024, 720);
         stage.setTitle("SOCIO");
         stage.setScene(scene);
         stage.setResizable(false);
+        mainStage = stage;
         stage.show();
     }
     public static void main(String[] args) {
