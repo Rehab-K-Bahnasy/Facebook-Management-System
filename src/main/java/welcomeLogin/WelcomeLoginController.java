ba11d0187e4ebe92083b1eb2b965b0ebda347f77
abdllrhmanzedan <abdom.zedan@gmail.com>
2023-12-06 04:42:59 +0200

    Added switching scenes between login and create account
    Signed-off-by: abdllrhmanzedan <abdom.zedan@gmail.com>

--- src/main/java/welcomeLogin/WelcomeLoginController.java
+++ src/main/java/welcomeLogin/WelcomeLoginController.java
@@ -1,11 +1,19 @@
 package welcomeLogin;
 
 import dataManager.DataManager;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.EventObject;
 
 public class WelcomeLoginController {
     @FXML
@@ -16,9 +24,9 @@ public class WelcomeLoginController {
     private TextField passwordText;
     @FXML
     private CheckBox showPassword;
+
     public void passwordVisibility() {
-        if(showPassword.isSelected())
-        {
+        if (showPassword.isSelected()) {
             passwordText.setText(passwordField.getText());
             passwordText.setVisible(true);
             passwordField.setVisible(false);
@@ -32,7 +40,7 @@ public class WelcomeLoginController {
     public void login() {
         String inputEmail = emailField.getText();
         String inputPassword = passwordField.getText();
-        if(showPassword.isSelected())
+        if (showPassword.isSelected())
             inputPassword = passwordText.getText();
         Alert alert;
 //        if(!DataManager.isValidUserCredentials(inputEmail,inputPassword))
@@ -51,7 +59,12 @@ public class WelcomeLoginController {
 //        //else: switch scene to the user home page
     }
 
-    public void createNewAccount() {
-
+    public void createNewAccount(ActionEvent event) throws IOException {
+        WelcomeLogin.fxmlLoader = new FXMLLoader(WelcomeLogin.class.getResource("CreateAccountScene.fxml"));
+        WelcomeLogin.root = WelcomeLogin.fxmlLoader.load();
+        WelcomeLogin.mainStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        WelcomeLogin.scene = new Scene(WelcomeLogin.root);
+        WelcomeLogin.mainStage.setScene(WelcomeLogin.scene);
+        WelcomeLogin.mainStage.show();
     }
 }
