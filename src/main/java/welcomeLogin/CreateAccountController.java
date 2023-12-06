4910a3e85dcdabf7d823752a0ead2698929f928f
ruby <rehab.khaled.bahnasy@gmail.com>
2023-12-06 21:17:14 +0200

    Finished GUI Completely design

--- src/main/java/welcomeLogin/CreateAccountController.java
+++ src/main/java/welcomeLogin/CreateAccountController.java
@@ -16,11 +16,35 @@ import java.util.EventObject;
 
 public class CreateAccountController {
     @FXML
-    private PasswordField passwordField;
+    private PasswordField password_field;
     @FXML
-    private TextField passwordText;
+    private PasswordField confirm_password_field;
     @FXML
-    private CheckBox showPassword;
+    private TextField password_text;
+    @FXML
+    private TextField confirm_password_text;
+    @FXML
+    private CheckBox show_password;
+
+    public void passwordVisibility() {
+        if (show_password.isSelected()) {
+            password_text.setText(password_field.getText());
+            confirm_password_text.setText(confirm_password_field.getText());
+            password_field.setVisible(false);
+            confirm_password_field.setVisible(false);
+            password_text.setVisible(true);
+            confirm_password_text.setVisible(true);
+            return;
+        }
+
+        password_field.setText(password_text.getText());
+        confirm_password_field.setText(confirm_password_text.getText());
+        password_text.setVisible(false);
+        confirm_password_text.setVisible(false);
+        password_field.setVisible(true);
+        confirm_password_field.setVisible(true);
+
+    }
     public void toLogin (ActionEvent event) throws IOException {
         WelcomeLogin.switchToScene(event,"WelcomeScene.fxml");
     }
