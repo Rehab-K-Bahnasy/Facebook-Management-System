4aa1845943719f8e230949765fcae72b1df50c66
ruby <rehab.khaled.bahnasy@gmail.com>
2023-12-06 15:24:44 +0200

    Added show password design.

--- src/main/java/welcomeLogin/CreateAccountController.java
+++ src/main/java/welcomeLogin/CreateAccountController.java
@@ -4,15 +4,19 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class CreateAccountController {
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField passwordText;
+    @FXML
+    private CheckBox showPassword;
     public void createNewAccount(ActionEvent event) throws IOException {
         WelcomeLogin.fxmlLoader = new FXMLLoader(WelcomeLogin.class.getResource("WelcomeScene.fxml"));
         WelcomeLogin.root = WelcomeLogin.fxmlLoader.load();
@@ -20,5 +24,16 @@ public class CreateAccountController {
         WelcomeLogin.scene = new Scene(WelcomeLogin.root);
         WelcomeLogin.mainStage.setScene(WelcomeLogin.scene);
         WelcomeLogin.mainStage.show();
+    }
+    public void passwordVisibility() {
+        if (showPassword.isSelected()) {
+            passwordText.setText(passwordField.getText());
+            passwordText.setVisible(true);
+            passwordField.setVisible(false);
+            return;
+        }
+        passwordField.setText(passwordText.getText());
+        passwordField.setVisible(true);
+        passwordText.setVisible(false);
     }
 }
