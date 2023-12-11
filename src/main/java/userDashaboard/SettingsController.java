70b3bd2660f6e4392f1a933b2d373801988dc3a9
abdllrhmanzedan <abdom.zedan@gmail.com>
2023-12-11 16:31:43 +0200

    Finished Settings GUI, Functionality:(not finished)
    Signed-off-by: abdllrhmanzedan <abdom.zedan@gmail.com>

--- src/main/java/userDashaboard/SettingsController.java
+++ src/main/java/userDashaboard/SettingsController.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,7 +15,31 @@ import java.io.IOException;
 public class SettingsController {
     @FXML
     private void cancelChanges(ActionEvent event) throws IOException {
-        HomePageController.switchToHomePage(event);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Cancel changes");
+        alert.setHeaderText("Any changes made won't be saved");
+        alert.setContentText("Are you sure you want to cancel?");
+        var result = alert.showAndWait();
+
+        if (result.isPresent()) {
+            if (result.get() == ButtonType.OK) {
+                HomePageController.switchToHomePage(event);
+            }
+        }
+    }
+    @FXML
+    private void saveChanges(ActionEvent event) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Save changes");
+        alert.setHeaderText("Any changes made will be saved");
+        alert.setContentText("Are you sure you want to save?");
+        var result = alert.showAndWait();
+
+        if (result.isPresent()) {
+            if (result.get() == ButtonType.OK) {
+                HomePageController.switchToHomePage(event);
+            }
+        }
     }
     public static void switchToSettings(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SettingsController.class.getResource("SettingsScene.fxml"));
